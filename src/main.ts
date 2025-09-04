8682f1fd0b4b09437e611b95429c56166b65613c
Alvien01 <alviantampan0@gmail.com>
2025-09-04 17:06:05 +0700

    add all function crud user

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+//import createApp from Vue
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+//import component App
+import App from './App.vue';
+
+//import VueQueryPlugin
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
+//import config router
+import routes from './routes';
+
+//create App Vue
+const app = createApp(App);
+
+//use VueQueryPlugin
+app.use(VueQueryPlugin);
+
+//use routes on vue
+app.use(routes);
+
+app.mount('#app');
